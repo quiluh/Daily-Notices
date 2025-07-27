a759935c575a7c3a66b5c373ab1a37c9ec8166dc
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-07-28 11:54:45 +1200

    coded register route

--- app.py
+++ app.py
@@ -40,7 +40,30 @@ def LogIn():
             pass # CODE THIS LATER
     elif request.method == "GET":
         return render_template("login.html")
+    
+@app.route("/register",methods=["GET","POST"]) # STORE USERNAME APPENDED TO PASSWORD FOR EXTRA SECURITY
+def Register():
+    if request.method == "POST":
+        name = request.form["name"]
+        code = request.form["code"]
+        username = request.form["username"]
+        password = request.form["password"]
 
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                # CHECK IF USERNAME ALREADY EXISTS
+                cursor.execute("SELECT * from teachers where username=%s",(username,))
+                user = cursor.fetchone()
+                if user is None:
+                    pass # CODE THIS LATER
+                else:
+                    cursor.execute("INSERT INTO teachers (name,code,username,password) VALUES (%s,%s,%s,%s)",(name,code,username,password)) 
+                    connection.commit()
+                    return redirect("/")       
+        
+    elif request.method == "GET":
+        return render_template("register.html")
+    
 @app.route("/add")
 def Add():
     pass
